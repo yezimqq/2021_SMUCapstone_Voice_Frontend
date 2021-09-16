f88fc44b91bada88c136d56eab09adfa94c37288
HeooSeungYeon <76529417+HeooSeungYeon@users.noreply.github.com>
2021-09-17 02:22:59 +0900

    Update Login, Signup, Navigation

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from '@react-navigation/native';
 import { theme } from './theme';
 import Navigation from './navigations';
-<<<<<<< HEAD
-
-=======
 import { UserProvider } from './contexts';
->>>>>>> master
 
 const App = () => {
   return (
     <ThemeProvider theme = {theme}>
-<<<<<<< HEAD
-      <StatusBar backgroundColor = {theme.statusbar} barStyle = "light-content"/>
-      <Navigation />
-=======
       <UserProvider>
         <StatusBar backgroundColor = {theme.statusbar} barStyle = "light-content"/>
         <Navigation />
       </UserProvider>
->>>>>>> master
     </ThemeProvider>
   );
 }
