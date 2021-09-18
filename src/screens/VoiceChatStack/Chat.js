a50fec5a72cddae743ee7e2cbb14cbad6118a2d8
HeooSeungYeon <76529417+HeooSeungYeon@users.noreply.github.com>
2021-09-19 04:30:55 +0900

    Update Chat.js
    Update chat screens

--- src/screens/VoiceChatStack/Chat.js
+++ src/screens/VoiceChatStack/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback, useLayoutEffect} from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
-import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Bubble, GiftedChat, Send, Actions, Composer, InputToolbar } from 'react-native-gifted-chat';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -38,7 +39,8 @@ const Chat = ({ navigation }) => {
         setMessages([
             {
                 _id: 1,
-                text: '안녕? 무슨 일 있니?',
+                text: '',
+                audio: 'voice.mp3',
                 createdAt: new Date(),
                 user: {
                     _id: 2,
@@ -50,6 +52,7 @@ const Chat = ({ navigation }) => {
                 _id: 2,
                 text: null,
                 createdAt: null,
+                audio: 'voice.mp3',
                 user: {
                     _id: 1,
                     name: 'React Native',
@@ -68,18 +71,55 @@ const Chat = ({ navigation }) => {
             <Bubble
                 {...props}
                 wrapperStyle={{
+                    left: {
+                        backgroundColor: 'white',
+                    },
                     right: {
-                        backgroundColor: '#2e64e5'
-                    }
-                }}
-                textStyle={{
-                    right: {
-                        color: '#fff'
+                        backgroundColor: '#ffe97d',
                     }
                 }}
             />
         );
+    };
+
+    /* 보류
+    const renderComposer = (props) => {
+        return (
+            <View style={styles.rowContainer}>
+                <Composer {...props} />
+                <Send {...props}>
+                    <MaterialCommunityIcons
+                        name="send-circle"
+                        style={styles.textInputIcon}
+                        size={32}
+                        color="#2e64e5"
+                    />
+                </Send>
+                <TouchableOpacity onPress={alert('안녕')}>
+                    <FontAwesome 
+                        name="microphone"
+                        style={styles.textInputIcon} 
+                        size={32}
+                    />  
+                </TouchableOpacity>
+            </View>
+        );
+    };
+
+    const renderActions = (props) => {
+        <View>
+            <Actions
+                {...props}
+                icon={() =>
+                    <FontAwesome 
+                        name="microphone"
+                        style={styles.textInputIcon} 
+                        size={32}
+                    />}
+            />
+        </View>
     }
+    */
 
     return (
         <GiftedChat
@@ -91,6 +131,9 @@ const Chat = ({ navigation }) => {
             renderBubble={renderBubble}
             alwaysShowSend
             scrollToBottom
+            //renderComposer={renderComposer}
+            //renderActions={renderActions}
+            //renderMessage={renderMessage}
         />
     );
 };
@@ -104,11 +147,16 @@ const styles = StyleSheet.create({
 
     rowContainer: {
         flexDirection: 'row',
+        alignItems: 'center',
     },
 
     headerIcon: {
         margin: 5,
     },
+
+    textInputIcon: {
+        margin: 5,
+    }
 
 });
 
