defcfc715787fa0a5fda5e81bc035c4b2b424a9c
HeooSeungYeon <76529417+HeooSeungYeon@users.noreply.github.com>
2021-09-21 20:42:36 +0900

    Update with firebase

--- src/screens/VoiceChatStack/ChatList.js
+++ src/screens/VoiceChatStack/ChatList.js
@@ -1,27 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Platform, StyleSheet, Text, View, TouchableOpacity, Image, FlatList, Dimensions } from 'react-native'; 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { images } from '../../images';
+import { DB } from '../../../utils/firebase';
 
-//예시 data
-const Messages = [
-    {
-        id: '1',
-        userName: '할머니',
-        userProfile: images.normal,
-        messageText: '마음이 너무 아프구나...',
-    },
-    
-    {
-        id: '2',
-        userName: '최준',
-        userProfile: images.blue,
-        messageText: '준이의 가장 찬란했던 순간을 함께 해줘서 고마워요.'
-    }
-]
+const ChatList = ({ navigation, route }) => {
+    const [channels, setChannels] = useState([]);
 
-const ChatList = ({ navigation }) => {
     useEffect(() => {
         navigation.setOptions({
             headerRight: () => (
@@ -45,30 +30,39 @@ const ChatList = ({ navigation }) => {
                             style={styles.headerIcon}
                         />
                     </TouchableOpacity>
-
                 </View>
             ),
         });
-    });
+
+        const unsubscribe = DB.collection('channels')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(snapshot => {
+                const list = [];
+                snapshot.forEach(doc => {
+                    list.push(doc.data());
+                });
+                setChannels(list);
+            });
+        
+        return () => unsubscribe();
+    }, []);
 
     return (
         <View style={styles.container}>
             <FlatList
-                data={Messages}
+                data={channels}
                 keyExtractor={item=>item.id}
                 renderItem={({item}) => (
                     <TouchableOpacity 
-                        onPress={() => navigation.navigate('Chat', {userName: item.userName})} 
+                        onPress={() => navigation.navigate('Chat', {name: item.name})} 
                         style={styles.chatRoom}>
                         <View style={styles.rowContainer}>
-                            <Image source={item.userProfile} style={styles.image} />
+                            <Image source={{ uri: item.image }} style={styles.image} />
                                 <View style={styles.colContainer}>
-                                    <Text style={styles.chatroomText}>{item.userName}</Text>
-                                    <Text numberOfLines={1} style={styles.chatroomText}>{item.messageText}</Text>
-                                    
+                                    <Text style={styles.nameText}>{item.name}</Text>
+                                    <Text style={styles.messageText}>{item.message}</Text>   
                                 </View>
                         </View>
-
                     </TouchableOpacity>
                 )}
             />
@@ -113,11 +107,19 @@ const styles = StyleSheet.create({
         marginLeft: 10,
     },
 
-    chatroomText: {
+    nameText: {
         fontSize: 16,
         marginLeft: 20,
         paddingTop: 10,
         width: 250,
+    },
+
+    messageText: {
+        fontSize: 14,
+        marginLeft: 30,
+        paddingTop: 10,
+        width: 250,
+        opacity: .50,
     }
 
 });
